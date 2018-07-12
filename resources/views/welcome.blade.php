3dd0ff946944a9fb36eb9d4024caf81fe660fe1d
Rasheed Rahman <ra1da35ma@gmail.com>
2018-07-12 11:43:09 +0100

    Basic Laravel Auth
    Signed-off-by: Rasheed Rahman <ra1da35ma@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>Laravel</title>
+	<title>Cregital Test</title>
 
 	<!-- Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -72,14 +72,14 @@
 				<a href="{{ url('/home') }}">Home</a>
 			@else
 				<a href="{{ route('login') }}">Login</a>
-				<a href="{{ route('register') }}">Register</a>
+				{{--<a href="{{ route('register') }}">Register</a>--}}
 			@endauth
 		</div>
 	@endif
 
 	<div class="content">
 		<div class="title m-b-md">
-			Laravel
+			Cregital Test
 		</div>
 
 		<div class="links">
