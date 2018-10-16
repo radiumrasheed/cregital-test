7df2cadb88d5e8d9dff591b86e1e3531f42050b3
Tobaino1 <tobainocycle@yahoo.com>
2018-10-16 23:31:08 +0100

    member controller
    Signed-off-by: Tobaino1 <tobainocycle@yahoo.com>

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+use Illuminate\Support\Facades\Auth;
+
 Route::get('/', function () {
 	return view('welcome');
 });
@@ -20,6 +22,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resources([
+    'member' => 'MemberController' ,
 	'companies' => 'CompaniesController',
 	'employees' => 'EmployeesController'
 ]);
