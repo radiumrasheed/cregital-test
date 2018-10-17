25723855d0691571b7054f048bc648da1b0eab95
Rasheed Rahman <ra1da35ma@gmail.com>
2018-10-17 07:20:25 +0100

    remove old components
    Signed-off-by: Rasheed Rahman <ra1da35ma@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -12,19 +12,18 @@
 */
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 	return view('welcome');
-});
+})->name('welcome');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resources([
-    'member' => 'MemberController' ,
-	'companies' => 'CompaniesController',
-	'employees' => 'EmployeesController'
+	'member' => 'MemberController'
 ]);
 
 // todo Add more routes
