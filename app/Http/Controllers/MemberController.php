ff707207d23f4d6a89d3a1884019c281a2aff3ed
Rasheed Rahman <ra1da35ma@gmail.com>
2018-10-17 07:22:09 +0100

    create member function
    Signed-off-by: Rasheed Rahman <ra1da35ma@gmail.com>

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -4,82 +4,102 @@ namespace App\Http\Controllers;
 
 use App\Member;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Session;
 
 class MemberController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return view('member.list');
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		return view('member.list');
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('member.create');
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		return view('member.create');
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Member  $member
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Member $member)
-    {
-        return view('home');
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Member  $member
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Member $member)
-    {
-        //
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 *
+	 * @return \Illuminate\Http\RedirectResponse
+	 */
+	public function store(Request $request)
+	{
+		// todo validation
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Member  $member
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Member $member)
-    {
-        //
-    }
+		$member = Member::create($request->all());
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Member  $member
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Member $member)
-    {
-        //
-    }
+		Session::flash('message', 'This is a message!');
+		Session::flash('alert-class', 'alert-danger');
+
+		return Redirect::to('welcome')->with('message', 'Member registered successfully');
+	}
+
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  \App\Member $member
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show(Member $member)
+	{
+		return view('home');
+	}
+
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  \App\Member $member
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit(Member $member)
+	{
+		//
+	}
+
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 * @param  \App\Member              $member
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, Member $member)
+	{
+		//
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  \App\Member $member
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy(Member $member)
+	{
+		//
+	}
 }
--- app/Member.php
+++ app/Member.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Member extends Model
 {
-    //
+	protected $fillable = [
+		'districts',
+		'surname',
+		'first_name',
+		'password',
+		'gender',
+		'phone_number',
+		'email',
+		'residential_address',
+		'business_category',
+		'business_name',
+		'business_location',
+		'discount_permitted',
+		'payment_method',
+		'subscription_mode',
+		'monthly_subscription_rates',
+		'quarterly_subscription_rates',
+		'annually_subscription_rates',
+		'rc_number',
+		'delivery_services',
+		'name_of_care_group_leader',
+		'care_group',
+		'telephone',
+	];
 }
