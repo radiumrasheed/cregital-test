dee1432bf7068fedcd7b9d2366d9a0efbcc2704d
Tobaino1 <tobainocycle@yahoo.com>
2018-10-18 13:41:10 +0100

    card enrollment table

---
+++ database/migrations/2018_10_17_132913_create_card_enrollments_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCardEnrollmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('card_enrollments', function (Blueprint $table) {
+
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('email_address');
+            $table->string('church_group');
+            $table->string('district');
+            $table->date('dob');
+            $table->string('gender');
+            $table->string('surname');
+            $table->string('firstname');
+            $table->string('middlename');
+            $table->string('mobile_number');
+            $table->string('residential_address');
+            $table->string('bvn');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('card_enrollments');
+    }
+}
