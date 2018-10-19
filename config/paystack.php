f188f11ad7030fcd3542ba5da88e720bff3039f8
Rasheed Rahman <ra1da35ma@gmail.com>
2018-10-19 14:04:43 +0100

    fix paystack config

--- config/paystack.php
+++ config/paystack.php
@@ -1,38 +1,29 @@
 <?php
 
-/*
- * This file is part of the Laravel Paystack package.
- *
- * (c) Prosper Otemuyiwa <prosperotemuyiwa@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 return [
 
-    /**
-     * Public Key From Paystack Dashboard
-     *
-     */
-    'publicKey' => getenv('pk_test_c76443ffe45b12f272001d815061625b57925bb0'),
+	/**
+	 * Public Key From Paystack Dashboard
+	 *
+	 */
+	'publicKey' => getenv('PAYSTACK_PUBLIC_KEY'),
 
-    /**
-     * Secret Key From Paystack Dashboard
-     *
-     */
-    'secretKey' => getenv('sk_test_5c17328e76ececa8a0dfa711cab2ce6fb44e7104'),
+	/**
+	 * Secret Key From Paystack Dashboard
+	 *
+	 */
+	'secretKey' => getenv('PAYSTACK_SECRET_KEY'),
 
-    /**
-     * Paystack Payment URL
-     *
-     */
-    'paymentUrl' => getenv('https://api.paystack.co'),
+	/**
+	 * Paystack Payment URL
+	 *
+	 */
+	'paymentUrl' => getenv('PAYSTACK_PAYMENT_URL'),
 
-    /**
-     * Optional email address of the merchant
-     *
-     */
-    'merchantEmail' => getenv('platinny10@gmail.com'),
+	/**
+	 * Optional email address of the merchant
+	 *
+	 */
+	'merchantEmail' => getenv('MERCHANT_EMAIL'),
 
 ];
